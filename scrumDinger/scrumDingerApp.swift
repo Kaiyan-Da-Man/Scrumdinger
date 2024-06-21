35c729b2d22b900ff5243e773935e82d978b476a
Bryan Tang <bryanktang@gmail.com>
2024-06-21 16:49:19 -0500

    Initial Commit

---
+++ scrumDinger/scrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumDingerApp.swift
+//  scrumDinger
+//
+//  Created by Bryan Tang on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
